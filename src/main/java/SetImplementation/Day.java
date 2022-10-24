ae178fe209e282bdf6dbe2bfdacffb267b3a050e
Nandita Hazra <hazranandita2022@gmail.com>
2022-10-24 15:46:10 +0530

    Added Java Enum: Day.java Added Java program: SpecialPurposeSetImplementation.java

---
+++ src/main/java/SetImplementation/Day.java
@@ -0,0 +1,22 @@
+package SetImplementation;
+
+/* An enum type is
+* a special data type
+* that enables for a variable
+* to be a set of
+* predefined constants. */
+
+// Enum type of days of week.
+public enum Day
+{
+    /* As the days of week
+    * are constants,
+    * they are in uppercase. */
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+}
